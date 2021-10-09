0835422bb5146fce56f8030b106449a5bd21cb8e
PavelEremeev <pe19950520@yandex.ru>
2021-10-09 20:20:20 +0300

    making mainpage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,16 +3,14 @@ import { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Header from '../Header/Header'
+import Main from '../Main/Main'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Header></Header>
-        <Route exact path="/">
-        </Route>
-      </Switch>
+      <Header></Header>
+      <Main></Main>
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,2 +1,46 @@
 import react from "react";
-import Main from './Main.js'
+import './Main.css'
+
+export default function Main() {
+
+    return (
+        <section className="main">
+            <section className="sets">
+                <div className="sets__container">
+                    <div className="sets__image"></div>
+                    <h1 className="sets__title">СЕТЫ</h1>
+
+                </div>
+            </section>
+            <section className="rolls">
+                <div className="rolls__container">
+                    <div className="rolls__image1"></div>
+                    <h2 className="rolls__text">РОЛЛЫ</h2>
+                </div>
+                <div className="rolls__container">
+                    <div className="rolls__image1 rolls__image2"></div>
+                    <h2 className="rolls__text">СУШИ</h2>
+                </div>
+                <div className="rolls__container">
+                    <h2 className="rolls__text">ЖАРЕННЫЕ РОЛЛЫ</h2>
+                    <div className="rolls__image1 rolls__image3"></div>
+
+                </div>
+                <div className="rolls__container">
+                    <h2 className="rolls__text">ЗАПЕЧЕННЫЕ РОЛЛЫ</h2>
+                    <div className="rolls__image1 rolls__image4"></div>
+
+                </div>
+            </section>
+            <section className="wok">
+                <div className="wok__container">
+                    <h2 className="wok__title">ВОК</h2>
+                    <h2 className="wok__title">КОНСТРУК</h2>
+                    <h2 className="wok__title">ТОР</h2>
+                    <h3 className="wok__subtitle">СОБЕРИ СВОЙ</h3>
+                    {/* <div className="wok__image"></div> */}
+                </div>
+            </section>
+        </section>
+    )
+}
