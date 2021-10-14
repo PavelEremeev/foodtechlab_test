0b66b2522c26394d1d63c28d177116cd80843f6e
PavelEremeev <pe19950520@yandex.ru>
2021-10-14 18:33:41 +0300

    fix Links. HashRouter done.ready for deploy

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -22,7 +22,6 @@ function App() {
           </Route>
         </Switch>
         <Footer></Footer>
-
       </div>
     </HashRouter>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,29 @@
 import react from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 export default function Header() {
 
-    const link = '/'
 
 
     return (
         <header className='header'>
 
             <div className='header__container'>
-                <a href={link} target='#' className='header__logo'></a>
+                <Link to='/' className='header__logo'></Link>
                 <div className='header__user-container'>
                     <div className='header__nav-container'>
-                        <a href={link} target='#' className='header__text'>Москва</a>
+                        <Link to='/' className='header__text'>Москва</Link>
                         <p className='header__text header__text-info'>8-800-550-30-30</p>
                     </div>
-                    <a href={link} target='#' className='header__text header__text-cabinet'>Личный кабинет</a>
+                    <Link to='/' className='header__text header__text-cabinet'>Личный кабинет</Link>
                 </div>
             </div>
             <div className='header__info-container'>
                 <div className='header__about-container'>
                     <div className='header__nav-container'>
-                        <a href={link} target='#' className='header__text'>О компании</a>
-                        <a href={link} target='#' className='header__text'>Условия доставки</a>
+                        <Link to='/' className='header__text'>О компании</Link>
+                        <Link to='/' className='header__text'>Условия доставки</Link>
                     </div>
                     <a href='https://xn--80adjkr6adm9b.xn--p1ai/static/sv/Polzovatelskoe_soglashenie_servisov_GK_SushiVesla.pdf'
                         target='#' className='header__text'>Пользовательское соглашение</a>
